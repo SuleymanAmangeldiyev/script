389205102be7d616edf6791a4ca1cb0f23d1c451
Suleyman <suleyman20071998@gmail.com>
2022-07-12 22:18:35 +0300

    a new

---
+++ script.js
@@ -0,0 +1,11 @@
+let age = 32;
+age = age + 1;
+let firstname = "suleyman";
+console.log(age);
+console.log(firstname);
+
+age /= 2;
+
+console.log(age);
+
+let username = window.prompt("What is your name?");
